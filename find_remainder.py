a63c0a5ebc76d8fd49ca05bd31635177097a67e6
Tejas Sharma <159901248+luciferim@users.noreply.github.com>
2025-08-01 20:08:21 +0530

    Add files via upload

---
+++ find_remainder.py
@@ -0,0 +1,8 @@
+# Program to find the remainder when a is divided by z'
+a = 17
+z = 5
+remainder = a % z
+print("The Remainder when", a, "is divided by", z, "=", remainder)
+
+# Output:
+# The Remainder when 17 is divided by 5 = 2

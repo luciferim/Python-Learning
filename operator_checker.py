cd4f32176ddcb0af3a1378d7cbad610eed937541
Tejas Sharma <159901248+luciferim@users.noreply.github.com>
2025-08-02 21:52:17 +0530

    Add files via upload

---
+++ operator_checker.py
@@ -0,0 +1,12 @@
+# Use Camparison to find out a whether a given variable a is greater than 'b' or not.
+# I'll take a = 34 and b = 80 .
+a = 34
+b = 80
+if a > b:
+    print("a is greater than b")
+else:
+    print("a is not greater than b")
+# This code checks if 'a' is greater than 'b' and prints the result.
+
+# Output :
+# a is not greater than b 

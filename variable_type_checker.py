88ff3bab3db21892d312e30bce780719cb766c45
Tejas Sharma <159901248+luciferim@users.noreply.github.com>
2025-08-02 21:51:13 +0530

    Add files via upload

---
+++ variable_type_checker.py
@@ -0,0 +1,32 @@
+# Check the Type of Variable assigned using type() function
+# Assigning values to variables
+a = 5
+b = 5.1
+c = "Hello World!"
+d = True
+e = [1, 2, 3, 4, 5]
+f = (6, 7, 8)
+g = {'name': 'Tejas', 'age': 18}
+h = None
+# Using type() function to check the type of each variable
+print("Type of a:", type(a))
+print("Type of b:", type(b))
+print("Type of c:", type(c))
+print("Type of d:", type(d))
+print("Type of e:", type(e))
+print("Type of f:", type(f))
+print("Type of g:", type(g))
+print("Type of h:", type(h))
+
+
+
+# Output the types of variables :
+
+# Type of a: <class 'int'>
+# Type of b: <class 'float'>
+# Type of c: <class 'str'>
+# Type of d: <class 'bool'>
+# Type of e: <class 'list'>
+# Type of f: <class 'tuple'>
+# Type of g: <class 'dict'>
+# Type of h: <class 'NoneType'>

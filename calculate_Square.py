33160094221f49a609662d726f9604b5d134e3ae
Tejas Sharma <159901248+luciferim@users.noreply.github.com>
2025-08-05 15:22:15 +0530

    Add files via upload

---
+++ calculate_Square.py
@@ -0,0 +1,14 @@
+# Program to calculate the square of a number entered by the user
+
+# Take input from the user
+number = float(input("Enter a number: "))
+
+# Calculate square
+square = number ** 2
+
+# Display the result
+print("The square of", number, "is:", square)
+
+# Output :
+# Enter a number: 25
+# The square of 25.0 is: 625.0
